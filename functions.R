ded51ca307bf742abbd183b7a000e4953ca0a11b
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2024-10-14 19:02:05 +0100

    Create functions.R

---
+++ functions.R
@@ -0,0 +1,35 @@
+# source ambition theme
+devtools::source_url("https://github.com/stefaniemeliss/ambition_theme/blob/main/ambition_theme.R?raw=TRUE")
+
+# combine to palette
+ambition_palette_bright <- c(cyan, coral, teal, purple, orange) # bright palette
+ambition_palette_accent <- c(yellow, blue, red)
+ambition_palette <- c(coral, teal, purple, orange, blue, red, cyan, yellow) # de-prioritise cyan and yellow
+
+# declare dominant and non-dominant colour in plots
+dominant_col <- coral
+nondominant_col <- navy
+
+table_desc <- function(data = df, group_var = "group", dep_var = "variable"){
+  
+  out <- rbind(
+    psych::describe(data[, dep_var]), # get descriptives whole sample
+    do.call("rbind",psych::describeBy(data[, dep_var], group = data[, group_var])) # get descriptives per group
+  )
+  # edit output
+  out$vars <- NULL
+  rownames(out)[1] <- "all"
+  out <- round(out, 3)
+  # print output
+  kbl(out, caption = paste0("Descriptives of variable '", dep_var,"' for whole sample and within each country")) %>%
+    kable_styling(bootstrap_options = c("striped", "hover", "condensed")) %>% 
+    htmltools::HTML() %>% 
+    print
+  cat("\n")
+}
+
+# function to determine outliers
+is_outlier_iqr <- function(x) {
+  # +/- 1.5*IQR
+  return(x < quantile(x, 0.25, na.rm = T) - 1.5 * IQR(x, na.rm = T) | x > quantile(x, 0.75, na.rm = T) + 1.5 * IQR(x, na.rm = T))
+}
