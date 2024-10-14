e46e8889174b60bc25e9b789ad6bf65730525eb3
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2024-10-14 18:17:32 +0100

    Update 00_download_government_data.R

--- 00_process/00_download_government_data.R
+++ 00_process/00_download_government_data.R
@@ -21,6 +21,11 @@ if (!dir.exists(dir_misc)) {
 # load libraries
 library(httr)
 
+# determine header information
+headers = c(
+  `user-agent` = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
+)
+
 ##### performance tables #####
 
 # determine output directory
@@ -32,11 +37,6 @@ if (!dir.exists(out_dir)) {
 # determine years of interest
 start <- 2010
 finish <- 2022
-
-# determine header information
-headers = c(
-  `user-agent` = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
-)
 
 
 for (year in start:finish) {
@@ -124,6 +124,28 @@ for (year in start:finish) {
   }
   
 }
+
+##### Get information about Dixons schools #####
+
+# specify URL
+url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/2939?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjoyOTM5fSwiTGFiZWwiOiJESVhPTlMgQUNBREVNSUVTIFRSVVNUIn1d0&downloadtype=csv&start=True"
+
+# retrieve information from URL 
+request <- GET(url = url, httr::add_headers(.headers=headers))
+request$headers$`content-type`
+request$headers$`content-disposition`
+request$headers$`content-encoding`
+
+# retrieve raw content from request
+bin <- content(request, "raw")
+
+# write binary data to file
+file_name <- file.path(dir_misc, "Dixons.zip")
+writeBin(bin, file_name)
+
+# unzip folder
+unzipped <- unzip(file_name, exdir = dir_misc)
+file.rename(unzipped, gsub("misc/", "misc/Dixons ", unzipped))
 
 ##### Get information about OAT schools #####
 
