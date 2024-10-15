d6b8b60bdbdca9e17cfec1714e1641cc4fdf4c57
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2024-10-16 00:59:37 +0100

    Update 00_download_data_government.R

--- 00_process/00_download_data_government.R
+++ 00_process/00_download_data_government.R
@@ -26,6 +26,72 @@ headers = c(
   `user-agent` = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
 )
 
+##### Get information about Dixons schools #####
+
+# specify URL
+url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/2939?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjoyOTM5fSwiTGFiZWwiOiJESVhPTlMgQUNBREVNSUVTIFRSVVNUIn1d0&downloadtype=csv&start=True"
+
+# retrieve information from URL 
+request <- GET(url = url, httr::add_headers(.headers=headers))
+request$headers$`content-type`
+request$headers$`content-disposition`
+request$headers$`content-encoding`
+
+# retrieve raw content from request
+bin <- content(request, "raw")
+
+# write binary data to file
+file_name <- file.path(dir_misc, "Dixons.zip")
+writeBin(bin, file_name)
+
+# unzip folder
+unzipped <- unzip(file_name, exdir = dir_misc)
+file.rename(unzipped, gsub("misc/", "misc/Dixons ", unzipped))
+
+##### Get information about OAT schools #####
+
+# specify URL
+url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/4106?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjo0MTA2fSwiTGFiZWwiOiJPUk1JU1RPTiBBQ0FERU1JRVMgVFJVU1QifV01&downloadtype=csv&start=True"
+
+# retrieve information from URL 
+request <- GET(url = url, httr::add_headers(.headers=headers))
+request$headers$`content-type`
+request$headers$`content-disposition`
+request$headers$`content-encoding`
+
+# retrieve raw content from request
+bin <- content(request, "raw")
+
+# write binary data to file
+file_name <- file.path(dir_misc, "OAT.zip")
+writeBin(bin, file_name)
+
+# unzip folder
+unzipped <- unzip(file_name, exdir = dir_misc)
+file.rename(unzipped, gsub("misc/", "misc/OAT ", unzipped))
+
+##### Get information about Lift schools #####
+
+# specify URL
+url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/2053?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjoyMDUzfSwiTGFiZWwiOiJMSUZUIFNDSE9PTFMifV01&downloadtype=csv&start=True"
+
+# retrieve information from URL 
+request <- GET(url = url, httr::add_headers(.headers=headers))
+request$headers$`content-type`
+request$headers$`content-disposition`
+request$headers$`content-encoding`
+
+# retrieve raw content from request
+bin <- content(request, "raw")
+
+# write binary data to file
+file_name <- file.path(dir_misc, "Lift.zip")
+writeBin(bin, file_name)
+
+# unzip folder
+unzipped <- unzip(file_name, exdir = dir_misc)
+file.rename(unzipped, gsub("misc/", "misc/Lift ", unzipped))
+
 ##### performance tables #####
 
 # determine output directory
@@ -125,50 +191,6 @@ for (year in start:finish) {
   
 }
 
-##### Get information about Dixons schools #####
-
-# specify URL
-url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/2939?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjoyOTM5fSwiTGFiZWwiOiJESVhPTlMgQUNBREVNSUVTIFRSVVNUIn1d0&downloadtype=csv&start=True"
-
-# retrieve information from URL 
-request <- GET(url = url, httr::add_headers(.headers=headers))
-request$headers$`content-type`
-request$headers$`content-disposition`
-request$headers$`content-encoding`
-
-# retrieve raw content from request
-bin <- content(request, "raw")
-
-# write binary data to file
-file_name <- file.path(dir_misc, "Dixons.zip")
-writeBin(bin, file_name)
-
-# unzip folder
-unzipped <- unzip(file_name, exdir = dir_misc)
-file.rename(unzipped, gsub("misc/", "misc/Dixons ", unzipped))
-
-##### Get information about OAT schools #####
-
-# specify URL
-url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/4106?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjo0MTA2fSwiTGFiZWwiOiJPUk1JU1RPTiBBQ0FERU1JRVMgVFJVU1QifV01&downloadtype=csv&start=True"
-
-# retrieve information from URL 
-request <- GET(url = url, httr::add_headers(.headers=headers))
-request$headers$`content-type`
-request$headers$`content-disposition`
-request$headers$`content-encoding`
-
-# retrieve raw content from request
-bin <- content(request, "raw")
-
-# write binary data to file
-file_name <- file.path(dir_misc, "OAT.zip")
-writeBin(bin, file_name)
-
-# unzip folder
-unzipped <- unzip(file_name, exdir = dir_misc)
-file.rename(unzipped, gsub("misc/", "misc/OAT ", unzipped))
-
 ##### School pupils and their characteristics (2023-24) ##### 
 
 url <- "https://content.explore-education-statistics.service.gov.uk/api/releases/60f096b8-6ed9-4e9e-97ee-2ca83867d51e/files"
@@ -241,3 +263,9 @@ writeBin(bin, file_name)
 unzipped <- unzip(file_name, exdir = file.path(dir_data, gsub(".zip", "", tmp)))
 file.remove(file_name)
 
+# extract postcodes
+tmp <- read.csv(file = file.path(dir_data, "school-pupils-and-their-characteristics_2023-24", "supporting-files", "spc_school_level_underlying_data.csv"))
+tmp <- as.data.frame(unique(tmp$school_postcode))
+
+write.table(tmp, file.path(dir_misc, "school_postcodes_spc_2023-24.csv"), 
+            row.names = F, col.names = F, sep = ",", quote = F)
