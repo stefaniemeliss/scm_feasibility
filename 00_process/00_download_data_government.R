8debac9f5271b69c00ce1ad6c1fcf6ae18b5a1a8
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2024-11-05 12:05:42 +0000

    Update 00_download_data_government.R

--- 00_process/00_download_data_government.R
+++ 00_process/00_download_data_government.R
@@ -25,6 +25,11 @@ library(httr)
 
 devtools::source_url("https://github.com/stefaniemeliss/scm_feasibility/blob/main/functions.R?raw=TRUE")
 
+# determine header information
+headers = c(
+  `user-agent` = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
+)
+
 
 ##### performance tables #####
 
@@ -128,11 +133,6 @@ webscrape_government_data(dir_out =  file.path(dir_data, "special-educational-ne
 
 ##### Get information about Dixons schools #####
 
-# determine header information
-headers = c(
-  `user-agent` = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
-)
-
 # specify URL
 url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/2939?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjoyOTM5fSwiTGFiZWwiOiJESVhPTlMgQUNBREVNSUVTIFRSVVNUIn1d0&downloadtype=csv&start=True"
 
@@ -187,4 +187,23 @@ writeBin(bin, file_name)
 # unzip folder
 unzipped <- unzip(file_name, exdir = dir_misc)
 file.rename(unzipped, gsub("misc/", "misc/Lift ", unzipped))
+
+##### Get information about Our Lady and All Saints Catholic Multi Academy Company ("the MAC") #####
+
+# specify URL
+url <- "https://get-information-schools.service.gov.uk/Downloads/Download/Group/17473?state=W3siUm91dGVOYW1lIjoiU2VhcmNoIiwiUm91dGVWYWx1ZXMiOnsiU2VsZWN0ZWRUYWIiOiJHcm91cHMifSwiTGFiZWwiOiJTZWFyY2gifSx7IlJvdXRlTmFtZSI6Ikdyb3VwRGV0YWlscyIsIlJvdXRlVmFsdWVzIjp7ImlkIjoxNzQ3M30sIkxhYmVsIjoiT1VSIExBRFkgQU5EIEFMTCBTQUlOVFMgQ0FUSE9MSUMgTVVMVEkgQUNBREVNWSBDT01QQU5ZIn1d0&downloadtype=csv&start=True"
+
+# retrieve information from URL 
+request <- GET(url = url, httr::add_headers(.headers=headers))
+
+# retrieve raw content from request
+bin <- content(request, "raw")
+
+# write binary data to file
+file_name <- file.path(dir_misc, "MAC.zip")
+writeBin(bin, file_name)
+
+# unzip folder
+unzipped <- unzip(file_name, exdir = dir_misc)
+file.rename(unzipped, gsub("misc/", "misc/MAC ", unzipped))
 
