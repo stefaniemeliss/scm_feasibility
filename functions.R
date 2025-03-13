931d2deea8b4ee0e8a319f52ae494414ce88d60d
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2025-03-13 17:39:33 +0000

    add source code for process_data_scm ()

--- functions.R
+++ functions.R
@@ -12,6 +12,211 @@ insert_slash <- function(number) {
   sub("(\\d{4})(\\d{2})", "\\1/\\2", number)
 }
 
+process_data_scm <- function(id_treated = "id_treated", 
+                             dv = "pupil_to_qual_teacher_ratio",
+                             var1 = "fte_avg_age",
+                             var2 = "pnpupfsm_e",
+                             regions = NULL,
+                             copy_files = F){
+  
+  # This is a data pre-processing function for later synthetic control method (SCM) analysis using [id_treated] as the treated school.
+  # The script copies and loads data files, processes data for treated and control schools
+  # to create outcome and predictor time series.
+  
+  
+  #### SETUP ####
+  
+  # Load necessary libraries
+  library(kableExtra)
+  library(dplyr)
+  library(data.table)
+  
+  # Define directories based on the current working directory
+  dir <- normalizePath(dir, winslash = "/")
+  dir_data <- file.path(dir, "data")
+  dir_misc <- file.path(dir, "misc")
+  
+  # Export the root and subdirectory to the global environment
+  assign("dir_data", dir_data, envir = .GlobalEnv)
+  assign("dir_misc", dir_misc, envir = .GlobalEnv)
+  
+  if(copy_files){
+    
+    # Copy data files from the source directory to the target directory
+    file.copy(
+      file.path(gsub("scm_feasibility", "edu_stats", dir_data), "data_swf.csv"),
+      dir_data,
+      overwrite = TRUE
+    )
+    file.copy(
+      file.path(gsub("scm_feasibility", "edu_stats", dir_data), "data_pupils.csv"),
+      dir_data,
+      overwrite = TRUE
+    )
+    file.copy(
+      file.path(gsub("scm_feasibility", "edu_stats", dir_data), "data_establishments_search.csv"),
+      dir_data,
+      overwrite = TRUE
+    )
+  }
+  
+  # Load data from CSV files into data tables
+  swf <- fread(file = file.path(dir_data, "data_swf.csv"))
+  pup <- fread(file = file.path(dir_data, "data_pupils.csv"))
+  est <- fread(file = file.path(dir_data, "data_establishments_search.csv"), na.strings = "")
+  
+  #### DETERMINE AVAILABLE TIMESERIES DATA ####
+  
+  # Identify years for which there are observations for treated school
+  data_avail_dv <- swf %>%
+    filter(!is.na(get(dv))) %>%
+    filter(laestab == id_treated) %>%
+    select(time_period, laestab, paste(dv)) %>%
+    distinct(time_period) %>%
+    pull(time_period)
+  
+  # Identify years for which there are observations for treated school
+  data_avail_age <- swf %>%
+    filter(!is.na(get(var1))) %>%
+    filter(laestab == id_treated) %>%
+    select(time_period, laestab, paste(var1)) %>%
+    distinct(time_period) %>%
+    pull(time_period)
+  
+  # Identify years for which there are observations for treated school
+  data_avail_fsm <- pup %>%
+    filter(!is.na(get(var2))) %>%
+    filter(laestab == id_treated) %>%
+    select(time_period, laestab, paste(var2)) %>%
+    distinct(time_period) %>%
+    pull(time_period)
+  
+  # combine, excluding duplicates
+  data_avail <- intersect(data_avail_dv, intersect(data_avail_age, data_avail_fsm))
+  
+  
+  #### PROCESS DATA FOR ESTABLISHMENTS ####
+  
+  # Get data for the treated school based on the ID
+  est_treated <- est %>%
+    filter(laestab == id_treated) %>%
+    mutate(status = "treated") %>%
+    as.data.frame()
+  
+  # overwrite region with default if not previously specified
+  if (is.null(regions)) {
+    regions <- unique(c(est_treated$gor_name))
+  }
+  
+  # Get donor pool data excluding the treated school
+  est_cont <- est %>%
+    filter(
+      laestab != id_treated,
+      phaseofeducation_name %in% unique(c(est_treated$phaseofeducation_name)),
+      gor_name %in% regions,
+      ! parliamentaryconstituency_name %in% unique(c(est_treated$parliamentaryconstituency_name)),
+      ! grepl("Boarding school", boarders_name),
+      admissionspolicy_name != "Selective"
+    ) %>%
+    mutate(status = "untreated") %>%
+    as.data.frame()
+  
+  # Save unique lists of laestab and urn
+  list_laestab <- c(unique(est_cont[, "laestab"]), unique(est_treated[, "laestab"]))
+  
+  #### CREATE OUTCOME DATASET ####
+  
+  # Filter SWF data to create outcome dataset
+  z <- swf[laestab %in% list_laestab & time_period %in% data_avail]
+  
+  # Remove rows for years for which the treated school has no data, 
+  # rows with NA for the dependent variable and age predictor 
+  # and add observation count
+  z <- z %>%
+    filter(!is.na(get(dv))) %>%
+    group_by(laestab) %>%
+    mutate(
+      obs_count_dv = sum(!is.na(get(dv))),
+      obs_count_var1 = sum(!is.na(get(var1)))
+    ) %>%
+    ungroup()
+  
+  # Filter for rows where observation count matches the treated ID and select columns
+  z <- z %>%
+    filter(obs_count_dv == unique(z$obs_count_dv[z$laestab == id_treated])) %>%
+    filter(obs_count_var1 == unique(z$obs_count_var1[z$laestab == id_treated])) %>%
+    select(time_period, laestab, school, pupil_to_qual_teacher_ratio, pupil_to_qual_unqual_teacher_ratio, fte_avg_age) %>%
+    group_by(laestab) %>%
+    arrange(laestab, desc(time_period)) %>%
+    mutate(school = first(school)) %>%
+    ungroup() %>%
+    as.data.frame()
+  
+  # Update list of laestab numbers based on the dependent dviable
+  check <- z %>% group_by(laestab) %>% summarise(n = n()) # identify estabs with more than 1 idaci decile (school has moved locations)
+  id_remove <- check$laestab[check$n != check$n[check$laestab == id_treated]] # identify estab numbers
+  list_laestab_dv <- unique(z$laestab)
+  list_laestab_dv <- setdiff(list_laestab_dv, id_remove)# update list_laestab_dv to exclude the ids that should be removed
+  rm(check)
+  
+  #### CREATE PREDICTOR DATASET ####
+  
+  # Filter SWF data to create outcome dataset
+  x <- pup[laestab %in% list_laestab_dv & time_period %in% data_avail]
+  
+  # Remove rows for years for which the treated school has no data, 
+  # rows with NA for the dependent variable and age predictor 
+  # and add observation count
+  x <- x %>%
+    filter(!is.na(get(var2))) %>%
+    group_by(laestab) %>%
+    mutate(
+      obs_count_var2 = sum(!is.na(get(var2)))
+    ) %>%
+    ungroup()
+  
+  # Filter for rows where observation count matches the treated ID and select columns
+  x <- x %>%
+    filter(obs_count_var2 == unique(x$obs_count_var2[x$laestab == id_treated])) %>%
+    select(time_period, laestab, urn, pnpupfsm_e) %>%
+    arrange(laestab, desc(time_period)) %>%
+    as.data.frame()
+  
+  # combine outcome and predictor
+  df <- merge(z, x, by = c("laestab", "time_period"))
+  
+  # arrange data
+  df <- df %>%
+    relocate(urn, .after = laestab) %>%
+    arrange(laestab, time_period) %>%
+    mutate(
+      # # change name to include laestab to navigate duplicates
+      # school = paste(laestab, school),
+      # add slash and use as string
+      time_period_str = insert_slash(time_period),
+      # remove the last two digits
+      time_period = as.numeric(substr(time_period, 0, 4))
+    )
+  
+  # export school name
+  id_name <- unique(df$school[df$laestab == id_treated])
+  assign("id_name", id_name, envir = .GlobalEnv)
+  
+  # save information about school to environment
+  if (nrow(est_treated) > 1) {
+    # if school has more than 1 URN linked to the same LAESTAB
+    est_treated <- subset(est_treated, est_treated$establishmentstatus_name == "Open")
+  }
+  assign("est_treated", est_treated, envir = .GlobalEnv)
+  
+  # clean up a little
+  rm(swf, pup, est)
+  gc()
+  
+  return(df)
+}
+
+
 # Function to determine the status of an establishment in a given academic year
 get_establishment_status <- function(data, laestab, academic_year_start) {
   # Define the start and end dates of the academic year
@@ -307,11 +512,8 @@ grid_search_scpi <- function(df, param_grid, use_parallel = FALSE, cv = FALSE) {
       if (grepl("same", params$region.filter[[1]])) (regions <- unlist(c(df_region[df_region$laestab == id_treated, c("same")])))
       if (grepl("neighbouring", params$region.filter[[1]])) (regions <- unlist(c(df_region[df_region$laestab == id_treated, c("same", "neighbouring")])))
       
-      # define script level variables
-      show_sd_crit = F
-      
-      # Load and execute the process_data.R script
-      source(file = file.path(dir, "00_process", "03_process_data_scm.R"))
+      # process data
+      df <- process_data_scm()
 
       # determine ids of control schools
       id_cont <- unique(df$laestab[df$laestab != id_treated])
