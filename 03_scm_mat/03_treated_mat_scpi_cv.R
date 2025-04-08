437544a288814993b7acf712c1c2f7e45b70d386
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2025-04-08 02:17:15 +0100

    update grid

--- 03_scm_mat/03_treated_mat_scpi_cv.R
+++ 03_scm_mat/03_treated_mat_scpi_cv.R
@@ -96,29 +96,85 @@ uid_treated <- 2939
 
 # define phases to loop through
 phases <- c("mixed", "Secondary", "Primary")
+p = 1 # debug
 
-for (phase in phases) {
+
+### define grid options, applicable to all phases ###
+
+# define options for donor pool
+min.years.obs.options = c(4, 6, 8)
+min.years.obs.options = c(8)
+min.schools.per.mat.options = c(2, 3, 4)
+min.schools.per.timeperiod.options = c(2, 3, 4)
+
+regions.options = list(c("Yorkshire and the Humber", "North West"), c("Yorkshire and the Humber"))
+regions.options = list(c("Yorkshire and the Humber", "North West"))
+
+exclude.northwest.options = c(TRUE, FALSE)
+
+# Define timeseries
+
+period.avail <- c(2010:2023)
+period.post <- c(2024:2026)
+
+period.avail.cv <- c(2010:2023)
+period.post.cv <- c(2022:2023)
+
+# Define options for outcome variable
+outcome.var.options <- c("pupil_to_qual_teacher_ratio") # Outcome variable
+
+# Define options for features
+features.options <- list(
+  # no DV
+  c("pnpupfsm_e", "fte_avg_age"), 
+  # DV included
+  ## raw
+  c("pupil_to_qual_teacher_ratio", "pnpupfsm_e", "fte_avg_age"))
+
+# Define possible covariate adjustments
+cov.adj.options <- list(
+  NULL,
+  list(c("constant")),
+  list(c("trend")),
+  list(c("constant", "trend"))
+)
+
+# Define possible options for including a constant
+constant.options <- c(TRUE, FALSE)
+
+# Define cointegrated data options
+cointegrated.data.options <- c(TRUE, FALSE)
+
+# Define options on how to constrain the weight matrix
+w.constr.options = list(
+  list(name = "simplex", p = "L1", lb = 0, Q = 1, dir = "=="), # default SCM
+  # list(name = "lasso", p = "L1", lb = -Inf, Q = 1, dir = "<="), # default lasso
+  # list(                p = "L1", lb = 0, Q = 1, dir = "<="), # lasso, lower bound changed to 0
+  list(name = "L1-L2", p = "L1-L2", lb = 0, Q = 1, Q2 = 1, dir = "==/<=") # default L1-l2
+)
+
+# define options for cross validation
+cross.val.options <- c(TRUE, FALSE)
+
+
+
+for (p in 1:length(phases)) {
+  
+  phase = phases[p]
   
   # define different filter option for each phase #
   
   if (phase == "mixed") {
+    
     # define options for filtering
     filter.options <- list(
-      "NULL",  # This will be interpreted as NULL later
-      "! time_period %in% c(201011, 201112)"
+      "! laestab %in% c(3802008)",  # This will be interpreted as NULL later
+      "! laestab %in% c(3802008) & ! time_period %in% c(201011, 201112)"
     )
     
-    # define options for donor pool
-    min.years.obs.options = c(4, 6, 8)
-    min.schools.per.mat.options = c(2, 3, 4)
-    min.schools.per.timeperiod.options = c(2, 3, 4)
-    
-    regions.options = list(c("Yorkshire and the Humber", "North West"), c("Yorkshire and the Humber"))
-    
     exclude.single.phase.options = c(TRUE, FALSE)
-    exclude.northwest.options = c(TRUE, FALSE)
     
-    filter_phase = "Not applicable"
+    filter_phase = c("Not applicable", "16 plus")
     
   } else if (phase == "Secondary") {
     
@@ -128,83 +184,22 @@ for (phase in phases) {
       "! time_period %in% c(201011, 201112, 201213, 201314)"
     )
     
-    # define options for donor pool
-    min.years.obs.options = c(4, 6, 8)
-    min.schools.per.mat.options = c(2, 3, 4)
-    min.schools.per.timeperiod.options = c(2, 3, 4)
-    
-    regions.options = list(c("Yorkshire and the Humber", "North West"))
-    
     exclude.single.phase.options = c(FALSE)
-    exclude.northwest.options = c(TRUE, FALSE)
     
     filter_phase = unique(groups$phaseofeducation_name)[!grepl("econdary", unique(groups$phaseofeducation_name))]
+    
   } else if (phase == "Primary") {
     
     # define options for filtering
     filter.options <- list(
-      "! time_period %in% c(201011, 201112, 201213, 201314)",
-      "! time_period %in% c(201011, 201112, 201213, 201314, 201415)"
+      "! laestab %in% c(3802008) & ! time_period %in% c(201011, 201112, 201213, 201314)",
+      "! laestab %in% c(3802008) & ! time_period %in% c(201011, 201112, 201213, 201314, 201415)"
     )
     
-    # define options for donor pool
-    min.years.obs.options = c(4, 6, 8)
-    min.schools.per.mat.options = c(2, 3, 4)
-    min.schools.per.timeperiod.options = c(2, 3, 4)
-    
-    regions.options = list(c("Yorkshire and the Humber", "North West"), c("Yorkshire and the Humber"))
-    
     exclude.single.phase.options = c(FALSE)
-    exclude.northwest.options = c(TRUE, FALSE)
     
     filter_phase = unique(groups$phaseofeducation_name)[!grepl("imary", unique(groups$phaseofeducation_name))]
   }
-  
-  ####################################
-  ### define grid
-  
-  
-  # Define timeseries
-  years.avail <- c(2010:2023)
-  period.post <- c(2022:2023)
-  
-  years.avail <- setdiff(years.avail, period.post)
-  
-  # Define option for pre-timeseries
-  period.pre.options <- list(c(sort(years.avail)))
-  
-  # Define options for outcome variable
-  outcome.var.options <- c("pupil_to_qual_teacher_ratio") # Outcome variable
-  
-  # Define options for features
-  features.options <- list(
-    # no DV
-    c("pnpupfsm_e", "fte_avg_age"), 
-    # DV included
-    ## raw
-    c("pupil_to_qual_teacher_ratio", "pnpupfsm_e", "fte_avg_age"))
-  
-  # Define possible covariate adjustments
-  cov.adj.options <- list(
-    NULL,
-    list(c("constant")),
-    list(c("trend")),
-    list(c("constant", "trend"))
-  )
-  
-  # Define possible options for including a constant
-  constant.options <- c(TRUE, FALSE)
-  
-  # Define cointegrated data options
-  cointegrated.data.options <- c(TRUE, FALSE)
-  
-  # Define options on how to constrain the weight matrix
-  w.constr.options = list(
-    list(name = "simplex", p = "L1", lb = 0, Q = 1, dir = "=="), # default SCM
-    # list(name = "lasso", p = "L1", lb = -Inf, Q = 1, dir = "<="), # default lasso
-    # list(                p = "L1", lb = 0, Q = 1, dir = "<="), # lasso, lower bound changed to 0
-    list(name = "L1-L2", p = "L1-L2", lb = 0, Q = 1, Q2 = 1, dir = "==/<=") # default L1-l2
-  )
   
   
   # Create initial parameter grid
@@ -215,27 +210,28 @@ for (phase in phases) {
     swf.filter.idx = 1:length(filter.options),
     regions = I(regions.options),
     exclude.single.phase = exclude.single.phase.options,
-    exclude.northwest = exclude.northwest.options,
-    filter_phase = I(list(filter_phase)),
+    # exclude.northwest = exclude.northwest.options,
+    filter.phase = I(list(filter_phase)),
     
     outcome.var = outcome.var.options,
     features = I(features.options),
     cov.adj = I(cov.adj.options),
     # period.pre = I(period.pre.options), # define using ifelse!!
+    # period.post = I(list(period.post)), # define using ifelse!!
+    period.avail = NA, # define using ifelse!!
     period.pre = NA, # define using ifelse!!
-    period.post = I(list(period.post)),
+    period.post = NA, # define using ifelse!!
     # cointegrated.data = I(cointegrated.data.options),
     # constant = I(constant.options),
     w.constr = w.constr.options,
+    cross.val = cross.val.options,
     stringsAsFactors = FALSE
   )
   
-  
-  # remove the options that don't include North West to begin with and then exclude north west later (redundant)
-  param_grid <- param_grid[! (!grepl("North West", param_grid$regions)& param_grid$exclude.northwest == T), ]
-  
-  # remove options where all years are used but min_schools_per_timeperiod is larger than 2
-  param_grid <- param_grid[ !(param_grid$swf.filter.idx == 1 & param_grid$min.schools.per.timeperiod > 2), ]
+  # change min.years.obs requirement based on CV filter
+  param_grid$min.years.obs <- ifelse(param_grid$cross.val, param_grid$min.years.obs-length(period.post.cv), param_grid$min.years.obs)
+
+  # translate SWF filter #
   
   # Add the actual filter expressions
   param_grid$swf.filter <- filter.options[param_grid$swf.filter.idx]
@@ -243,37 +239,35 @@ for (phase in phases) {
   # Remove the index column
   param_grid$swf.filter.idx <- NULL
   
-  # manually define period.pre based on filters
-  if (phase == "mixed") {
-    param_grid$period.pre <- ifelse(param_grid$swf.filter == "NULL", I(list(c(sort(years.avail)))), I(list(c(sort(setdiff(years.avail, c(2010:2011)))))))
-  } else if (phase == "Secondary") {
-    param_grid$period.pre <- ifelse(grepl("201314", param_grid$swf.filter), I(list(c(sort(setdiff(years.avail, c(2010:2013)))))), I(list(c(sort(setdiff(years.avail, c(2010:2011)))))))
-  } else if (phase == "Primary") {
-    param_grid$period.pre <- ifelse(grepl("201415", param_grid$swf.filter), I(list(c(sort(setdiff(years.avail, c(2010:2014)))))), I(list(c(sort(setdiff(years.avail, c(2010:2013)))))))
-  }
+  # Add if any schools should be exclude
+  param_grid$excl.outlier <- grepl("laestab", param_grid$swf.filter)
   
+  # extract any years to be excluded
+  param_grid$period.excl <- I(apply(param_grid, 1, function(row) {
+    extract_years_from_filter(row["swf.filter"])
+  }))
   
+  # define period.post and period.avail based on CV filter
+  param_grid$period.post <- ifelse(param_grid$cross.val, I(list(period.post.cv)), I(list(period.post)))
+  param_grid$period.avail <- ifelse(param_grid$cross.val, I(list(period.avail.cv)), I(list(period.avail)))
+  
+  # Calculate period.pre based on period.avail, period.post and period.excl.
+  param_grid$period.pre <- mapply(calculate_period_pre, param_grid$period.avail, param_grid$period.post, param_grid$period.excl, SIMPLIFY = FALSE)
+
   # tidy up grid #
   
-  # check if features include any ratios
-  param_grid$ratio <- sapply(1:nrow(param_grid), function(i){ grepl("ratio", param_grid$features[i]) })
   # check if outcome variable is included as feature
   param_grid$included <- sapply(1:nrow(param_grid), function(i){ grepl(param_grid$outcome.var[i], param_grid$features[i]) })
-  # remove if ratio is in features but the features do not have outcome variable included
-  param_grid$keep1 <- ifelse(param_grid$ratio & !param_grid$included, F, T)
-  param_grid <- param_grid[param_grid$keep1 == T, ]
   # remove if covariate adjustment is not NULL but outcome variable is not
-  param_grid$keep2 <- ifelse(grepl("t", param_grid$cov.adj) & !param_grid$included, F, T)
-  param_grid <- param_grid[param_grid$keep2 == T, ]
+  param_grid$keep <- ifelse(grepl("t", param_grid$cov.adj) & !param_grid$included, F, T)
+  param_grid <- param_grid[param_grid$keep == T, ]
   
   # remove cols
-  param_grid$keep1 <- NULL
-  param_grid$keep2 <- NULL
-  param_grid$ratio <- NULL
+  param_grid$keep <- NULL
   param_grid$included <- NULL
   
   # determine output filename
-  file_name <- file.path(dir, "03_scm_mat", paste0(file_stem, "_", tolower(phase), ".csv"))
+  file_name <- file.path(dir, "03_scm_mat", "interim", paste0(file_stem, "_", tolower(phase), ".csv"))
   
   run_gridsearch <- T
   
@@ -283,7 +277,7 @@ for (phase in phases) {
     start <- Sys.time()
     
     results <- grid_search_scpi_mat(param_grid = param_grid[, ],
-                                    cv = T)
+                                    cv = T, sim = T)
     
     print( Sys.time() - start )
     
